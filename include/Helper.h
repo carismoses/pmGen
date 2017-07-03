091fe8a6b251cbf7fc040f8a5c2bd468439d7d30
Caris Moses <caris.moses@mujin.co.jp>
2017-07-03 09:47:51 +0000

    Added all includes files for helper header

--- include/Helper.h
+++ include/Helper.h
@@ -1,31 +1,39 @@
 #ifndef PMG_HELPER_H
 #define PMG_HELPER_H
 
-#include <map>
+#include "TypeGen.h"
+#include "SlotTracker.h"
+#include "llvm/IR/Value.h"
+#include "llvm/IR/GlobalVariable.h"
+#include "llvm/IR/Metadata.h"
+#include "llvm/IR/Value.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/User.h"
+#include "llvm/IR/Constant.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/ADT/StringRef.h"
 #include <string>
+#include <map>
 
 namespace llvm {
 
-    /*
-      enum PrefixType {
-      GlobalPrefix,
-      LabelPrefix,
-      LocalPrefix,
-      NoPrefix
-      };
+    enum PrefixType {
+        GlobalPrefix,
+        LabelPrefix,
+        LocalPrefix,
+        NoPrefix
+    };
 
-    */
-    // class SlotTracker;
-    // class Value;
-    // class raw_ostream;
-    // class TypeGen;
-    // class Module;
-    // class User;
-    // class Constant;
-    // class MDNode;
+    class SlotTracker;
+    class Value;
+    class raw_ostream;
+    class TypeGen;
+    class Module;
+    class User;
+    class Constant;
+    class MDNode;
     class StringRef;
-    // class GlobalVariable;
+    class GlobalVariable;
     
     
     class ConStr {
@@ -39,60 +47,60 @@ namespace llvm {
         std::string getString(const StringRef name);
         ConStr *get();
     };
+    
+    class Helper {
 
-    /*
-      class Helper {
+    public:
+        static void test(raw_ostream &OS,StringRef Name);
 
-      public:
-      static void test(raw_ostream &OS,StringRef Name);
+        static SlotTracker *createSlotTracker(const Value *V);
 
-      static SlotTracker *createSlotTracker(const Value *V);
+        static const Module *getModuleFromVal(const Value *V);
+      
+        /// PrintLLVMName - Turn the specified name into an 'LLVM name', which is either
+        /// prefixed with % (if the string only contains simple characters) or is
+        /// surrounded with ""'s (if it has special chars in it).  Print it out.
+        static void PrintLLVMName(raw_ostream &OS, StringRef Name, PrefixType Prefix); 
+        static void PrintLLVMName(raw_ostream &OS, const GlobalVariable &GV);
 
-      static const Module *getModuleFromVal(const Value *V);
-      static void PrintLLVMName(raw_ostream &OS, StringRef Name, PrefixType Prefix); 
+        static void PrintEscapedString(StringRef Name,raw_ostream &Out);
 
-      static void PrintLLVMName(raw_ostream &OS, const GlobalVariable &GV);
+        static void WriteAsOperandInternal(
+                                           raw_ostream &Out, const GlobalVariable &GV,
+                                           TypeGen *TypePrinter,
+                                           SlotTracker *Machine,
+                                           const Module Context);
 
-      static void PrintEscapedString(StringRef Name,raw_ostream &Out);
-
-      static void WriteAsOperandInternal(
-      raw_ostream &Out, const GlobalVariable &GV,
-      TypeGen *TypePrinter,
-      SlotTracker *Machine,
-      const Module Context);
-
-      static const char *getPredicateText(unsigned predicate);
+        static const char *getPredicateText(unsigned predicate);
 	
-      static void WriteOptimizationInfo(raw_ostream &Out, const User *U);	
+        static void WriteOptimizationInfo(raw_ostream &Out, const User *U);	
 
-      static void WriteConstantInternal(
-      raw_ostream &Out,const Constant *CV,
-      TypeGen &TypePrinter,
-      SlotTracker *Machine,
-      const Module *Context);
+        static void WriteConstantInternal(
+                                          raw_ostream &Out,const Constant *CV,
+                                          TypeGen &TypePrinter,
+                                          SlotTracker *Machine,
+                                          const Module *Context);
 
-      static void WriteMDNodeBodyInternal(
-      raw_ostream &Out,const MDNode *Node,
-      TypeGen *TypePrinter,
-      SlotTracker *Machine,
-      const Module *Context);
+        static void WriteMDNodeBodyInternal(
+                                            raw_ostream &Out,const MDNode *Node,
+                                            TypeGen *TypePrinter,
+                                            SlotTracker *Machine,
+                                            const Module *Context);
 
-      static void WriteAsOperand(
-      raw_ostream &Out,const Value *V,
-      bool PrintType,const Module *Context);
+        static void WriteAsOperand(
+                                   raw_ostream &Out,const Value *V,
+                                   bool PrintType,const Module *Context);
 
-      static void InitBE(raw_ostream &Out,bool BorE);
+        static void InitBE(raw_ostream &Out,bool BorE);
 
-      static void InitGValue(
-      raw_ostream &Out,const GlobalVariable *GV,
-      TypeGen *TypePrinter,
-      SlotTracker *Machine,
-      const Module *Context);
+        static void InitGValue(
+                               raw_ostream &Out,const GlobalVariable *GV,
+                               TypeGen *TypePrinter,
+                               SlotTracker *Machine,
+                               const Module *Context);
 	
-      static void Formatting(std::string &s);
-      };
-    */
-
+        static void Formatting(std::string &s);
+    };
 }
 
 #endif
